f5ed925ef94bb3f296acb1906eb908deec999b07
spacemars <mroushey@gmail.com>
2013-04-27 16:49:25 -0400

    added stars

--- game/src/controller/BackgroundController.js
+++ game/src/controller/BackgroundController.js
@@ -2,9 +2,23 @@ function BackgroundController(main) {
     this.main = main;
     this.ground = new GroundPlane();
 
+    this.stars = [];
+
+    for( var i=0; i<100; i++){
+        var star = new Star();
+
+        this.stars.push(star);
+        this.main.add(star);
+    }
+
     //this.main.scene.add(this.ground.wire);
 }
 
 BackgroundController.prototype.update = function () {
     //this.ground.update();
+
+
+    for( var i=0; i<this.stars.length; i++){
+        this.stars[i].update();
+    }
 };
---
+++ game/src/gameObject/Star.js
@@ -0,0 +1,48 @@
+function Star() {
+    GameObject.call(this, this.buildMesh(), 0xffffff, 0xffffff);
+    this.wire.scale = new THREE.Vector3(1, 1, 1);
+    this.wireMat.opacity = 0.25;
+    this.solidMat.opacity = 0.5;
+
+    this.resetPos();
+
+    this.vel.x = 0;
+    this.vel.y = -20;
+    this.update();
+
+    this.solidMat.blending = THREE.AdditiveAlphaBlending;
+}
+
+Star.prototype = new GameObject();
+Star.prototype.constructor = Star;
+
+Star.prototype.update = function () {
+    GameObject.prototype.update.call(this);
+
+
+    if (this.pos.y < -2000) this.resetPos();
+};
+
+
+Star.prototype.resetPos = function () {
+    this.pos.x = Math.random() * 500 - 250;
+    this.pos.z = Math.random() * 500 - 250;
+    this.pos.y = 1000 + Math.random() * 4000;
+}
+
+Star.prototype.buildMesh = function () {
+    var geometry = new THREE.Geometry();
+
+    var w = 0.5;
+    var h = 200;
+
+    geometry.vertices.push(new THREE.Vector3(0, h, 0));
+    geometry.vertices.push(new THREE.Vector3(-w, h* 0.5, 0));
+    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+
+    geometry.computeBoundingSphere();
+
+    return geometry;
+};
